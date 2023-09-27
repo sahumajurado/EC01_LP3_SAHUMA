2897560e45803a08704d8b15738e7ef408c57f5e
sahumajurado <2113110059@untels.edu.pe>
2023-09-27 01:14:53 -0500

    Pregunta 1 y 2 listo por Sahuma

---
+++ Ejercicio01.py
@@ -0,0 +1,10 @@
+#Definir las variables
+a = int(input("Ingresar el valor de A: "))
+b = int(input("Ingresar el valor de B: "))
+#Cambiando el valor de las variables
+c = a
+a = b
+b = c
+#Cambio de valores
+print(f"El nuevo valor de A es: {a}")
+print(f"El nuevo valor de B es: {b}")
---
+++ Ejercicio02.py
@@ -0,0 +1,6 @@
+#Solicitando el valor de n al usuario
+n = int(input("Ingresar el valor de n: "))
+#Hallando la sumatorio
+sumatoria = (n * (n+1)) /2
+#Imprimiendo la respuesta
+print(f"La suma de los {n} primeros números naturales es: {sumatoria}")
